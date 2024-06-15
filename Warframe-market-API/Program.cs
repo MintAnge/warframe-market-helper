2e02a14898bd5c766b4d20eb894b3f61e4457243
MintAnge <CatMintFox@gmail.com>
2024-06-15 19:52:07 +0300

    Add API for Warframe Market

---
+++ Warframe-market-API/Program.cs
@@ -0,0 +1,5 @@
+﻿using Warframe_market_API;
+
+string s = "creeping_bullseye";
+var wmAPI = new WarframeMarketAPI();
+Console.WriteLine(await wmAPI.GetOrdersAsync(s));
---
+++ Warframe-market-API/WarframeMarketAPI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warframe_market_API
+{
+    internal class WarframeMarketAPI
+    {
+        public static HttpClient sharedClient = new()
+        {
+            BaseAddress = new Uri("https://api.warframe.market/v1/"),
+        };
+
+        public async Task<string> GetOrdersAsync(string s)
+        {
+            HttpRequestMessage m = new HttpRequestMessage(HttpMethod.Get, $"items/{s}/orders");
+            string n = await sharedClient.SendAsync(m).Result.Content.ReadAsStringAsync();
+            return n;
+        }
+
+        public async Task<string> GetAllItemsAsync()
+        {
+            HttpRequestMessage m = new HttpRequestMessage(HttpMethod.Get, "items" );
+            string n = await sharedClient.SendAsync(m).Result.Content.ReadAsStringAsync();
+            return n;
+        }
+
+        public async Task<string> GetItemAsync(string s)
+        {
+            HttpRequestMessage m = new HttpRequestMessage(HttpMethod.Get, $"items/{s}");
+            string n = await sharedClient.SendAsync(m).Result.Content.ReadAsStringAsync();
+            return n;
+        }
+    }
+}
