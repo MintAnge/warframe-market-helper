576e17ad2275cd97759a1fb30db815a9ca0e5148
MintAnge <CatMintFox@gmail.com>
2024-06-16 19:34:53 +0300

    Added API for warframe market

--- Warframe-market-API/Program.cs
+++ Warframe-market-API/Program.cs
@@ -1,5 +1,6 @@
-﻿using Warframe_market_API;
+﻿using System.Text.Json;
+using Warframe_market_API;
 
 string s = "creeping_bullseye";
 var wmAPI = new WarframeMarketAPI();
-Console.WriteLine(await wmAPI.GetOrdersAsync(s));
+Console.WriteLine((await wmAPI.GetOrdersAsync(s)).RootElement.GetProperty("payload").GetProperty("orders")[0].GetProperty("platinum").GetInt32());
--- Warframe-market-API/WarframeMarketAPI.cs
+++ Warframe-market-API/WarframeMarketAPI.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Warframe_market_API
@@ -12,12 +13,13 @@ namespace Warframe_market_API
         {
             BaseAddress = new Uri("https://api.warframe.market/v1/"),
         };
-
-        public async Task<string> GetOrdersAsync(string s)
+        public async Task<JsonDocument> GetOrdersAsync(string s)
         {
             HttpRequestMessage m = new HttpRequestMessage(HttpMethod.Get, $"items/{s}/orders");
-            string n = await sharedClient.SendAsync(m).Result.Content.ReadAsStringAsync();
-            return n;
+            HttpResponseMessage n = await sharedClient.SendAsync(m);
+            Stream rm = await n.Content.ReadAsStreamAsync();
+            var str = await WarframeMarketAPI.GetStream(rm);
+            return str;
         }
 
         public async Task<string> GetAllItemsAsync()
@@ -32,6 +34,12 @@ namespace Warframe_market_API
             HttpRequestMessage m = new HttpRequestMessage(HttpMethod.Get, $"items/{s}");
             string n = await sharedClient.SendAsync(m).Result.Content.ReadAsStringAsync();
             return n;
+        }
+
+        public static async Task<JsonDocument> GetStream(Stream json)
+        {
+            var v = await JsonDocument.ParseAsync(json);
+            return v;
         }
     }
 }
